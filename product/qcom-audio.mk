d55197cbe44db95564b35b6f75abab5b8d43a6f6
Scott Mertz <scott@cyngn.com>
2016-02-19 17:25:17 -0800

    ailsa_ii: disable low latency primary
    The akm codec can't handle it!
    
    Change-Id: I9d1bcf514b7280b01bf2f4e8ee4a2f0aef139f3e

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -18,10 +18,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
-
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
