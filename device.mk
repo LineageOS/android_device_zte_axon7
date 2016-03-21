d7dc1760125c0ae1cd747623e6bdaf0b9c335859
Praveen Chavan <pchavan@codeaurora.org>
2016-03-21 09:37:14 -0700

    axon7: mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    
    Bug: 27654762
    
    Change-Id: I9aba945e50bece5b94f4fd15e44bc84d50a41a0b

--- device.mk
+++ device.mk
@@ -330,6 +330,11 @@ PRODUCT_PACKAGES += \
     libshims_rild_socket \
     libshims_ims
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
