9bac538a4f4cc962c444ac140352a29342d9c1b0
c457 <android.c357@gmail.com>
2016-09-06 10:24:18 -0500

    ailsa_ii: Enable low latency audio

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -19,8 +19,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
 # Permissions
-#PRODUCT_COPY_FILES += \
-#    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
