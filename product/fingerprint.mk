b9d315b3cf1355288a5d2943954ffb1bfed62e75
c457 <android.c357@gmail.com>
2016-09-07 16:32:28 -0500

    ailsa_ii: Enable fingerprint again
    * Add missing blob

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,7 +1,7 @@
 # Fingerprint sensor
-#PRODUCT_PACKAGES += \
-#    init.fingerprint.goodix_fp.rc \
-#    fingerprintd
+PRODUCT_PACKAGES += \
+    init.fingerprint.goodix_fp.rc \
+    fingerprintd
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.qfp=false
