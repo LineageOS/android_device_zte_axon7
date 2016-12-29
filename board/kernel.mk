430c2443db079f078a3e43cc16b69116fc488aee
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-12-29 01:50:49 -0500

    board: Fix kernel path
    Change-Id: I3f1a931d5a970e52de7fa9f3d8de808438ad0494
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -9,6 +9,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/zte/tom
+TARGET_KERNEL_SOURCE := kernel/zte/msm8996
 TARGET_KERNEL_CONFIG := cyanogenmod_ailsa_ii_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
