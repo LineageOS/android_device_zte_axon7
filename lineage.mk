eeea910dc203c67b51cb0758f055a03d16477be6
Brandon McAnsh <brandon.mcansh@gmail.com>
2017-01-09 12:17:54 -0500

    axon7: Rebrand for LineageOS
    Change-Id: I10508e9f457e602a06aab4e3018bd312369d1b13
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2016 The CyanogenMod Project
+            (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +15,7 @@
 
 $(call inherit-product, device/zte/axon7/full_axon7.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_axon7
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo cm_axon7-eng
-add_lunch_combo cm_axon7-userdebug
