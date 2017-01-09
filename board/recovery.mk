8120fbf5da82ebea7319d0e22a53b70cb23f5c50
Brandon McAnsh <brandon.mcansh@gmail.com>
2017-01-09 16:19:51 -0500

    axon7: Add OTA asserts
    * Allow ailsa_ii to still pass ota asserting
    
    Change-Id: I6c2b388f5f2bceb58c8f4f3a1a3bc46e04a71ee3
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,5 +1,6 @@
 # Assertions
 TARGET_BOARD_INFO_FILE ?= device/zte/axon7/board-info.txt
+TARGET_OTA_ASSERT_DEVICE := ailsa_ii,axon7
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/zte/axon7/rootdir/etc/fstab.qcom
