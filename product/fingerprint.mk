882ab84974a139a761b0cf692073db03dfc784da
c457 <android.c357@gmail.com>
2017-01-21 09:00:56 -0600

    ailsa_ii: Use opensource Fingerprint daemon
    * We however lose ZTE fp wake acceleration

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,3 +1,7 @@
+# Fingerprintd
+PRODUCT_PACKAGES += \
+    fingerprintd
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
