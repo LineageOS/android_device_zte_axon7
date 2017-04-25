bb929d4511b3579ab9c1f5464db9b8e37c484ccc
Thierry Strudel <tstrudel@google.com>
2017-04-25 14:27:11 -0700

    axon7: fix android.hardware.gnss@1.0-service-qti
    Make service part of 'hal' class.
    Install files in vendor partition.
    Register instance as 'default' as expected by framework.
    
    Change-Id: I487fad4df872397ab76cea377e5304c956a756da
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- gps/android/Android.mk
+++ gps/android/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE := android.hardware.gnss@1.0-impl-qti
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_OWNER := qti
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := \
     AGnss.cpp \
     Gnss.cpp \
@@ -64,6 +65,7 @@ LOCAL_MODULE := android.hardware.gnss@1.0-service-qti
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_OWNER := qti
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_INIT_RC := android.hardware.gnss@1.0-service-qti.rc
 LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := \
