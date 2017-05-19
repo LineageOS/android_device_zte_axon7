50cc3fd3a8b68e212ffd5f5e5cf65594d759671c
Abhishek Arpure <aarpure@codeaurora.org>
2017-05-19 08:32:12 -0700

    axon7: Move hardware specific .rc files to /vendor/etc/init/hw
    The reason these files are moved to /vendor/etc/init/hw and
    not /vendor/etc/init is because 'init' scans the /vendor/etc/init folder
    for vendor service .rc fragments to import. That would either result
    in duplicate imports or out-of-order imports of these hardware
    specific .rc files and that *may* have unintended consequences.
    
    In order to preserve the current import order for MTP, we move all
    hardware specific .rc files to /vendor/etc/init/hw which is not
    autoscanned by init BUT leave the init.${ro.hardware}.rc (i.e.
    init.qcom.rc in this case) in root for 'init' to find.
    
    Change-Id: I14531da05e2a3a256f734f8de96e1f1969c753f9

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -30,7 +30,7 @@ LOCAL_MODULE       := init.msm.usb.configfs.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.msm.usb.configfs.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -38,7 +38,7 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -46,7 +46,7 @@ LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -62,7 +62,7 @@ LOCAL_MODULE       := init.qcom.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 # Copy the power config for recovery too
