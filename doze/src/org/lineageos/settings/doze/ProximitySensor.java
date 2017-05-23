82be73ab4fbd29ae67c22eefd4bc5b7a7aa47b84
LuK1337 <priv.luk@gmail.com>
2017-05-23 21:29:58 +0200

    axon7: Doze: Require non-wake up proximity sensor
    * getDefaultSensor(type) returns the first sensor
      and in case of capricorn it's wake-up one thus our
      doze package wouldn't work. Passing `false` as a second
      argument makes it work properly.
    
    Change-Id: If73bc43bc166945d27b95513c4255ae5a5590849

--- doze/src/org/lineageos/settings/doze/ProximitySensor.java
+++ doze/src/org/lineageos/settings/doze/ProximitySensor.java
@@ -45,7 +45,7 @@ public class ProximitySensor implements SensorEventListener {
     public ProximitySensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
     }
 
     @Override
