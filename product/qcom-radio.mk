a38c3ee79c2abc6cae40b1ebda27c1fb26184655
c457 <android.c357@gmail.com>
2017-05-24 14:43:20 -0500

    axon7: Updates from B25
    * Package Version A2017UV1.1.0B25 - NMF26V
    * qti-telephony-common.jar unmodified from ZTE
    * sensors.ssc.so unmodified from ZTE
    * Pin audio calibration database from B15 (B19+ breaks audio)
    
    Change-Id: I961ee1c52aa215e629f2a477708602d20c7edd01

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -26,6 +26,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
      persist.radio.calls.on.ims=true \
      persist.radio.csvt.enabled=false \
      persist.radio.custom_ecc=1 \
+     persist.radio.data_con_rprt=1 \
      persist.radio.flexmap_type=disabled \
      persist.radio.hw_mbn_update=1 \
      persist.radio.mt_sms_ack=20 \
@@ -68,3 +69,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
      persist.data.qmi.adb_logmask=0 \
      persist.data.wda.enable=true \
      persist.rmnet.data.enable=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+     persist.sys.ssr.restart_level=ALL_ENABLE \
+     persist.sys.ztelog.enable=0
