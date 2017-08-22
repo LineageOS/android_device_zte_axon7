8d70b4d3a0b2883a40450af222dcc8fff36251e3
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    axon7: Add basic HIDL manifest
     * This defines the interfaces for graphics and wifi
    
    Change-Id: I2c3aece8ad13613ec5b83496aa23d26ef238f45d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,9 @@ ENABLE_CPUSETS := true
 
 TARGET_USES_64_BIT_BINDER := true
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_BASE := 0x80000000
