5927461814964ebc46c75b1d406955c168b38ad3
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:11:27 +0200

    axon7: Build Camera HIDL HAL interface and provider
    Change-Id: Id845e815310027d0feb60c83b487f776d9175fa1

--- device.mk
+++ device.mk
@@ -149,6 +149,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
     Snap
 
 # Connectivity Engine support (CNE)
