ca7db86a015af3b1a9fbd17919c1ea3a32c20ae4
Luca Stefani <luca020400@lineageos.org>
2017-08-27 20:32:05 +0200

    axon7: Remove no longer used hostapd configs
    Change-Id: I0c53cbb1ba2610aac4349b6a0e66e23c75871d27

--- device.mk
+++ device.mk
@@ -302,9 +302,6 @@ PRODUCT_PACKAGES += \
     wifi_symlinks
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wcnss/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/wcnss/hostapd.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/wcnss/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/wcnss/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wcnss/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wcnss/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
