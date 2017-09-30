de442d3ac55641a01439ecf6fceb1fa48ad4f097
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 19:34:50 -0400

    axon7: Build netd HAL and netutils wrapper for netmgrd
    Change-Id: I93453934a2f3e06296eb4f7a62941b9b6abca703

--- device.mk
+++ device.mk
@@ -270,6 +270,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Net
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0 \
+    netutils-wrapper-1.0
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
