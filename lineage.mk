319dab8fbcd216e627d36ba5f8cd9dd1c1beb06e
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-10-02 18:50:25 +0200

    axon7: s@vendor/cm@vendor/lineage@g
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- lineage.mk
+++ lineage.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zte/axon7/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_axon7
