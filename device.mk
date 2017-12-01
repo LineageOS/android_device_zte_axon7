d683852fdae644daed6078861a3e4c02f619e013
TheScarastic <warabhishek@gmail.com>
2017-12-01 01:03:01 -0600

    axon7: Add custom Biometrics HIDL HAL
     * This fixes gdxfingerprint usage
    
     * Avoid notifying cancel hacks like https://github.com/AOSPA/android_framework$
       as they will conflict with the implementation in the biometricservice
    
     * Co-authored with @alexstsv2
    
    Change-Id: Ieb7b8843d2fa5f1699b432ac529fb656c608467d
    Signed-off-by: TheScarastic <warabhishek@gmail.com>

--- device.mk
+++ device.mk
@@ -242,7 +242,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service-custom \
+    fingerprint.msm8996
 
 # Media
 PRODUCT_COPY_FILES += \
