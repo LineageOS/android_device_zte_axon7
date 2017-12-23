72d30be2b54c5e91137a33fdc7ab3af80537077a
Bruno Martins <bgcngm@gmail.com>
2017-12-23 19:42:59 +0000

    axon7: Remove Broadcast Radio HAL impl
     * The Axon 7 doesn’t have support for
     * FM radio, so remove useless lib.
    
    Change-Id: I64f9f0f3fe403d4bd863ef214af24d40f312f9ba

--- device.mk
+++ device.mk
@@ -107,7 +107,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audiod \
     audio.a2dp.default \
