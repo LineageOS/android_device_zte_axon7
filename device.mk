356fe4895bf6205790e9dde837ae1a8fe3195762
Bruno Martins <bgcngm@gmail.com>
2017-12-24 02:29:51 +0000

    axon7: Move Wi-Fi files to vendor
    Change-Id: I7e95baa01490ec76cf8ddfa051f01fc9b0bc5c7a

--- device.mk
+++ device.mk
@@ -347,6 +347,6 @@ PRODUCT_PACKAGES += \
     wifi_symlinks
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wcnss/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wcnss/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wcnss/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+    $(LOCAL_PATH)/wcnss/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wcnss/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wcnss/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
