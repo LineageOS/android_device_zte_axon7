e009a443b1f6baae662df499d926aae9d3045413
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-12-26 04:35:20 +0100

    axon7: Replace IRQ balance path with $(TARGET_COPY_OUT_VENDOR)
     * It would get copied to system/vendor anyways
     * and "$(TARGET_COPY_OUT_VENDOR)" does the same

--- device.mk
+++ device.mk
@@ -221,7 +221,7 @@ PRODUCT_PACKAGES += \
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # IRSC
 PRODUCT_COPY_FILES += \
