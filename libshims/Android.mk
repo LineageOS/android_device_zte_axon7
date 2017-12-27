c8ce9ed9c962577c287a9c8def27856908a896e7
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-12-27 14:52:32 +0100

    axon7: Add camera shim
     * E HAL     : dlopen failed: cannot locate symbol "_ZN7android20DisplayEventReceiverC1Ev" referenced by "/system/lib/hw/camera.msm8996.so"..

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -44,3 +44,22 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MULTILIB := 64
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    camera_shim.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libgui \
+    libui
+
+LOCAL_C_INCLUDES := \
+    frameworks/native/include
+
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
