5c6d9c1ad99f8207cf32f068ae762e8dff1a4d7d
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-12-27 18:32:03 +0100

    axon7: Fix missing symbol for libFNVfbEngineHAL.so

--- libshims/camera_shim.cpp
+++ libshims/camera_shim.cpp
@@ -48,3 +48,5 @@ extern "C" void _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11Vsync
 extern "C" void _ZN7android20DisplayEventReceiverC1Ev() {
     _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceE;
 }
+
+extern "C" void _ZN7android13GraphicBuffer10reallocateEjjij() {}
