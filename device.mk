aab12a88ae68e1eb03968f986c506f447d3cf3a2
Bruno Martins <bgcngm@gmail.com>
2018-01-08 12:56:54 +0000

    axon7: Ship IPACM again
     * After all it was enabled again by Qualcomm.
    
     * Also define tetheroffload HALs in the manifest.
    
    Change-Id: Ifab33197d264d5eb46afb7eedc206030217beeb2
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- device.mk
+++ device.mk
@@ -233,6 +233,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common
 
+# IPA Manager
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
