5b9e311197bd07d038ae1536f813a1188766dc95
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2018-01-29 15:50:12 +0100

    Revert "axon7: Add IMS shim"
    This reverts commit 3980efa33126a36a72f327ad1342ff78d41dbaf6.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -35,18 +35,6 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := MediaBuffer.cpp
-
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui
-
-LOCAL_MODULE := libshims_ims
-LOCAL_MODULE_TAGS := optional
-LOCAL_MULTILIB := 64
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     camera_shim.cpp
 
