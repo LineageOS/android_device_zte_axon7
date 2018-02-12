a07352e7d5126ca54e84ebe72446f2e476bd5b89
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2018-02-13 00:28:37 +0100

    axon7: Remove WiFi stats support
     * Despite this device has a QCACLD2 WLAN card
       and the node that should return WiFi stats is available,
       it seems that the firmware doesn't fully supports it,
       causing freezes when trying to access the node.
       Just go with the HAL that provides no support for this
       feature.
    
     * ref: https://github.com/LineageOS/android_device_xiaomi_msm8996-common/commit/e850cd2e562462f38948ace3060dd825a8cd0db7#diff-581794583e89a660d8f68651b78d17f9
    
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- device.mk
+++ device.mk
@@ -325,7 +325,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 # QCOM
 PRODUCT_COPY_FILES += \
