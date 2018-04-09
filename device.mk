e9980fdd3cfc299758c8bb020abaf4cf0cac8371
Bruno Martins <bgcngm@gmail.com>
2018-04-09 23:07:57 +0100

    axon7: Drop USB HAL service
     * Axon 7 doesn't support dual role type-C,
       therefore there's no need to build it.
    
    Change-Id: Ie5b135b8920ae255a2054a04cb7326c68abad0cd

--- device.mk
+++ device.mk
@@ -403,10 +403,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
