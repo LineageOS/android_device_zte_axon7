1b95af9db0b9788598cda8abe3802924f3eaa510
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2018-04-26 17:40:20 +0200

    Revert "axon7: Add camera shim"
    This reverts commit c8ce9ed9c962577c287a9c8def27856908a896e7.

--- device.mk
+++ device.mk
@@ -163,7 +163,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
-    libshim_camera \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
