f582a750c71fe3edba2ccebd3315ec9fe20a6c27
c457 <android.c357@gmail.com>
2018-05-13 00:10:23 -0500

    amplifier: Fix L and R channel switching
    * Add support for testing different Amp versions Oreo, Nougat,
    and ZTE original bypass mode patch.

--- audio_amplifier/audio_amplifier.c
+++ audio_amplifier/audio_amplifier.c
@@ -16,6 +16,7 @@
 
 
 #include <time.h>
+#include <cutils/properties.h>
 #include <system/audio.h>
 #include <platform.h>
 
@@ -36,16 +37,14 @@
 
 #include <hardware/audio_amplifier.h>
 
-#define AUDIO_NORMAL 0
-#define AUDIO_VOICE 1
-#define AUDIO_MUSIC 2
+#define MUSIC 0
+#define VOICE 1
 
 typedef struct tfa9890_state {
     int streams;
     int in_call;
     int device;
     int mode;
-    int amp_mode;
     int reversed;
     int enabled;
     int calibration_done;
@@ -54,12 +53,19 @@ typedef struct tfa9890_state {
 typedef struct tfa9890_device {
     amplifier_device_t amp_dev;
     void *lib_ptr;
-    void (*set_mode)(int);
-    int (*speaker_on)(int mode);
-    void (*speaker_off)(void);
+    int (*ex_speaker_on)(int mode);
+    void (*ex_speaker_off)(void);
+    int (*ex_calibration)(int);
     void (*set_device)(int);
     int (*calibration)(int);
-    void (*reverse)(int);
+    void (*set_mode)(int);
+    int (*speaker_needed)(int);
+    void (*stereo_speaker_on)(void);
+    void (*stereo_speaker_off)(void);
+    int (*stereo_speaker_needed)(int,int);
+    void (*speaker_on_bypass)(void);
+    void (*speaker_off_bypass)(void);
+    void (*set_amp_state)(void *device, tfa9890_state_t rstat, int reinitialize);
     // Hold for all the things
     pthread_mutex_t amp_update;
     tfa9890_state_t state;
@@ -75,7 +81,6 @@ typedef struct tfa9890_device {
           "streams: %d(%d)\n\t"\
           "in_call: %d(%d)\n\t"\
           "mode: %d(%d)\n\t"\
-          "amp_mode: %d(%d)\n\t"\
           "reversed: %d(%d)\n",\
           __func__, label,\
           t.enabled, n.enabled,\
@@ -83,71 +88,67 @@ typedef struct tfa9890_device {
           t.streams, n.streams,\
           t.in_call, n.in_call,\
           t.mode, n.mode,\
-          t.amp_mode, t.amp_mode,\
           t.reversed, n.reversed\
     );\
 } while (0)
 
-void *tfa9890_update_amp(void *device) {
+#define LEFT_HANDLE 0
+#define RIGHT_HANDLE 1
+
+#define LEFT_IN 0
+#define RIGHT_IN 1
+
+void reverse(void *device, int reversed) {
     tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
-    tfa9890_state_t rstate = {0,0,0,0,0,0,0,0};
-    do {
-        bool reinitialize = false;
-        sem_wait(&tfa9890->trigger_update);
-        pthread_mutex_lock(&tfa9890->amp_update);
+    if(reversed) {
+        ALOGD("The function, it does nothing.");
+    }
+}
 
-        // Drain any other pending updates
-        while(sem_trywait(&tfa9890->trigger_update) != -1);
-
-        tfa9890_dump_state(tfa9890->state, rstate, "new(current)");
-
-        if (tfa9890->state.enabled != rstate.enabled) {
-            reinitialize = true;
+// The new way to power on and use the amp.  This should be the correct option
+void tfa9890_set_amp_state(void *device, tfa9890_state_t rstate, int reinitialize) {
+    tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
+    if (reinitialize) {
+        tfa9890->ex_speaker_off();
+        tfa9890->set_device(rstate.device);
+        reverse(tfa9890, tfa9890->state.reversed);
+    }
+    if(rstate.enabled && (rstate.streams || rstate.in_call)) {
+        int again = 1;
+        while (again && tfa9890->ex_speaker_on(rstate.mode)){
+            pthread_mutex_unlock(&tfa9890->amp_update);
+            usleep(5*1000);
+            ALOGD("%s: Failed turning amp on %d times", __func__, again);
+            again = (sem_trywait(&tfa9890->trigger_update) == -1) ? again + 1 : 0;
+            pthread_mutex_lock(&tfa9890->amp_update);
         }
-        rstate.enabled = tfa9890->state.enabled;
-
-        if (tfa9890->state.device != rstate.device) {
-            reinitialize = true;
+        if (!again) {
+            ALOGD("%s: Amp state was changed, triggering refresh", __func__);
+            sem_post(&tfa9890->trigger_update);
         }
-        rstate.device = tfa9890->state.device;
+    } else {
+        tfa9890->ex_speaker_off();
+    }
+}
 
-        if (!!tfa9890->state.streams != !!rstate.streams) {
-            reinitialize = true;
-        }
-        rstate.streams = tfa9890->state.streams;
-
-        if (tfa9890->state.in_call != rstate.in_call) {
-            reinitialize = true;
-        }
-        rstate.in_call = tfa9890->state.in_call;
-
-        rstate.mode = tfa9890->state.mode;
-
-        if (tfa9890->state.amp_mode != rstate.amp_mode) {
-            if (!reinitialize) {
-                tfa9890->set_mode(tfa9890->state.amp_mode);
-            }
-        }
-        rstate.amp_mode = tfa9890->state.amp_mode;
-
-        if (tfa9890->state.reversed != rstate.reversed) {
-            if (!reinitialize) {
-                tfa9890->reverse(tfa9890->state.reversed);
-            }
-        }
-        rstate.reversed = tfa9890->state.reversed;
-
-        if(rstate.enabled && (rstate.streams || rstate.in_call)) {
-            if (reinitialize) {
-                tfa9890->speaker_off();
-                tfa9890->set_device(rstate.device);
-                tfa9890->set_mode(rstate.amp_mode);
-                tfa9890->reverse(rstate.reversed);
-            }
-            int again = 1;
-            while (again && tfa9890->speaker_on(rstate.mode)){
+// Almost the same way we did it on the pre-o amp-hal.  The only change is in how we
+// handle turning the amp on when in a call, which didn't work properly on N anyways
+void tfa9890_set_amp_state_legacy(void *device, tfa9890_state_t rstate, int reinitialize) {
+    tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
+    if (reinitialize) {
+        tfa9890->speaker_needed(0);
+        tfa9890->stereo_speaker_off();
+        tfa9890->set_device(rstate.device);
+        reverse(tfa9890, tfa9890->state.reversed);
+    }
+    if(rstate.enabled && (rstate.streams || rstate.in_call)) {
+        int again = 1;
+        if (rstate.in_call) {
+            // Wait for a clock to become available from the modem
+            tfa9890->speaker_needed(1);
+            while (again && tfa9890->ex_speaker_on(rstate.mode)){
                 pthread_mutex_unlock(&tfa9890->amp_update);
-                usleep(5*1000);
+                usleep(1*1000);
                 ALOGD("%s: Failed turning amp on %d times", __func__, again);
                 again = (sem_trywait(&tfa9890->trigger_update) == -1) ? again + 1 : 0;
                 pthread_mutex_lock(&tfa9890->amp_update);
@@ -157,8 +158,107 @@ void *tfa9890_update_amp(void *device) {
                 sem_post(&tfa9890->trigger_update);
             }
         } else {
-            tfa9890->speaker_off();
+            tfa9890->set_mode(rstate.mode);
+            tfa9890->speaker_needed(1);
+            tfa9890->stereo_speaker_on();
         }
+    } else {
+        /*
+        if (!tfa9890->state.calibration_done) {
+            int ret = tfa9890->ex_calibration(0);
+            if (ret) {
+                if (ret != -1) {
+                    ALOGE("exTfa98xx_calibration: %d\n", ret);
+                } else {
+                    tfa9890->state.calibration_done = 1;
+                }
+            } else {
+                tfa9890->state.calibration_done = 1;
+            }
+        }*/
+        tfa9890->speaker_needed(0);
+        tfa9890->stereo_speaker_off();
+    }
+}
+
+// The 'original' amp hal method that kinda worked.
+void tfa9890_set_amp_state_bypass(void *device, tfa9890_state_t rstate, __attribute__((unused)) int reinit) {
+    tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
+    if (rstate.enabled) {
+        if (rstate.in_call) {
+            tfa9890->stereo_speaker_needed(1, 0);
+        } else {
+            tfa9890->stereo_speaker_needed(1, 1);
+        }
+        tfa9890->speaker_on_bypass();
+    } else {
+        tfa9890->stereo_speaker_needed(0, 0);
+        tfa9890->speaker_off_bypass();
+    }
+}
+
+#define AMP_CONTROL_NORMAL 0
+#define AMP_CONTROL_LEGACY 1
+#define AMP_CONTROL_BYPASS 2
+
+void tfa9890_switch_amp_control_mode(void *device) {
+    tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
+    int32_t amp_control_mode = property_get_int32("persist.axon7.amp_hal_control", 0);
+    switch(amp_control_mode) {
+        default:
+        case AMP_CONTROL_NORMAL:
+            if (tfa9890->set_amp_state != &tfa9890_set_amp_state) {
+                ALOGD("Using normal amp control");
+                tfa9890->set_amp_state = &tfa9890_set_amp_state;
+            }
+            break;
+        case AMP_CONTROL_LEGACY:
+            if (tfa9890->set_amp_state != &tfa9890_set_amp_state_legacy) {
+                ALOGD("Using legacy amp control");
+                tfa9890->set_amp_state = &tfa9890_set_amp_state_legacy;
+            }
+            break;
+        case AMP_CONTROL_BYPASS:
+            if (tfa9890->set_amp_state != &tfa9890_set_amp_state_bypass) {
+                ALOGD("Using bypass amp control");
+                tfa9890->set_amp_state = &tfa9890_set_amp_state_bypass;
+            }
+            break;
+    }
+}
+
+void *tfa9890_update_amp(void *device) {
+    tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
+    tfa9890_state_t rstate = {0,0,0,0,0,0,0,};
+    bool reinitialize = true;
+    do {
+        sem_wait(&tfa9890->trigger_update);
+        pthread_mutex_lock(&tfa9890->amp_update);
+
+        // Drain any other pending updates
+        while(sem_trywait(&tfa9890->trigger_update) != -1);
+
+        tfa9890_dump_state(tfa9890->state, rstate, "new(current)");
+
+
+        if (tfa9890->state.device != rstate.device) {
+            reinitialize = true;
+        }
+        rstate.device = tfa9890->state.device;
+        rstate.enabled = tfa9890->state.enabled;
+        rstate.streams = tfa9890->state.streams;
+        rstate.in_call = tfa9890->state.in_call;
+        rstate.mode = tfa9890->state.mode;
+        if (tfa9890->state.reversed != rstate.reversed) {
+            reinitialize = true;
+        }
+        rstate.reversed = tfa9890->state.reversed;
+
+        tfa9890->set_amp_state(device, rstate, reinitialize);
+        if (!rstate.enabled) {
+            tfa9890_switch_amp_control_mode(tfa9890);
+        }
+        reinitialize = false;
         pthread_mutex_unlock(&tfa9890->amp_update);
     } while(sem_trywait(&tfa9890->trigger_exit) == -1);
     return NULL;
@@ -197,9 +297,14 @@ static int tfa9890_set_output_devices(struct amplifier_device *device, uint32_t 
         case SND_DEVICE_OUT_VOICE_HANDSET:
             tfa9890->state.device = 1;
             break;
+        case SND_DEVICE_NONE:
+            break;
+
     }
-    tfa9890->state.reversed = (devices == SND_DEVICE_OUT_SPEAKER_REVERSE);
-    sem_post(&tfa9890->trigger_update);
+    if (devices != SND_DEVICE_NONE) {
+        tfa9890->state.reversed = (devices == SND_DEVICE_OUT_SPEAKER_REVERSE);
+        sem_post(&tfa9890->trigger_update);
+    }
     pthread_mutex_unlock(&tfa9890->amp_update);
     return 0;
 }
@@ -214,16 +319,13 @@ static int tfa9890_enable_output_devices(struct amplifier_device *device, uint32
         case SND_DEVICE_OUT_SPEAKER:
         case SND_DEVICE_OUT_SPEAKER_REVERSE:
             tfa9890->state.in_call = 0;
-            tfa9890->state.mode = 2;
             break;
         case SND_DEVICE_OUT_VOICE_SPEAKER:
             tfa9890->state.in_call = enable;
-            tfa9890->state.mode = 2;
             break;
         case SND_DEVICE_OUT_HANDSET:
         case SND_DEVICE_OUT_VOICE_HANDSET:
             tfa9890->state.in_call = enable;
-            tfa9890->state.mode = 1;
             break;
     }
     // 'enabled' should probably be tracked based on devices
@@ -237,17 +339,16 @@ static int tfa9890_set_mode(struct amplifier_device *device, audio_mode_t mode) 
     tfa9890_device_t *tfa9890 = (tfa9890_device_t*) device;
     pthread_mutex_lock(&tfa9890->amp_update);
     ALOGD("%s: %u\n", __func__, mode);
-    // Stock used 2 and 1 and 0, and all 3 seem to be the same... no clue.  Draken's log showed 2 for music
     switch (mode) {
         default:
             ALOGE("%s: Unhandled audio mode: %d\n", __func__, mode);
         case AUDIO_MODE_NORMAL:
         case AUDIO_MODE_RINGTONE:
-            tfa9890->state.amp_mode = AUDIO_MUSIC;
+            tfa9890->state.mode = MUSIC;
             break;
         case AUDIO_MODE_IN_CALL:
         case AUDIO_MODE_IN_COMMUNICATION:
-            tfa9890->state.amp_mode = AUDIO_VOICE;
+            tfa9890->state.mode = VOICE;
             break;
     }
     sem_post(&tfa9890->trigger_update);
@@ -312,6 +413,8 @@ static int tfa9890_module_open(const hw_module_t *module,
     tfa9890_dev->amp_dev.output_stream_standby = &tfa9890_output_stream_standby;
     tfa9890_dev->amp_dev.set_parameters = &tfa9890_set_parameters;
 
+    tfa9890_dev->state.mode = MUSIC;
+
     tfa9890_dev->lib_ptr = dlopen("libtfa9890.so", RTLD_NOW);
     if (!tfa9890_dev->lib_ptr) {
         ALOGE("%s:%d: Unable to open libtfa9890.so: %s",
@@ -321,12 +424,11 @@ static int tfa9890_module_open(const hw_module_t *module,
     }
 
     // Functions normally exported, or at least I can find reference to outside our hal.
-    *(void **)&tfa9890_dev->speaker_on = dlsym(tfa9890_dev->lib_ptr, "exTfa98xx_speakeron");
-    *(void **)&tfa9890_dev->speaker_off = dlsym(tfa9890_dev->lib_ptr, "exTfa98xx_speakeroff");
-    *(void **)&tfa9890_dev->reverse = dlsym(tfa9890_dev->lib_ptr, "exTfa98xx_LR_Switch");
-    *(void **)&tfa9890_dev->calibration = dlsym(tfa9890_dev->lib_ptr, "exTfa98xx_calibration");
-    if (!tfa9890_dev->speaker_on || !tfa9890_dev->speaker_off ||
-        !tfa9890_dev->reverse || !tfa9890_dev->calibration) {
+    *(void **)&tfa9890_dev->ex_speaker_on = dlsym(tfa9890_dev->lib_ptr, "exTfa98xx_speakeron");
+    *(void **)&tfa9890_dev->ex_speaker_off = dlsym(tfa9890_dev->lib_ptr, "exTfa98xx_speakeroff");
+    *(void **)&tfa9890_dev->ex_calibration = dlsym(tfa9890_dev->lib_ptr, "exTfa98xx_calibration");
+    if (!tfa9890_dev->ex_speaker_on || !tfa9890_dev->ex_speaker_off ||
+        !tfa9890_dev->ex_calibration) {
         ALOGE("%s:%d: Unable to find required symbols", __func__, __LINE__);
         dlclose(tfa9890_dev->lib_ptr);
         free(tfa9890_dev);
@@ -335,14 +437,25 @@ static int tfa9890_module_open(const hw_module_t *module,
 
 
     // Internal functions we aren't supposed to need to use directly
+    *(void **)&tfa9890_dev->speaker_needed = dlsym(tfa9890_dev->lib_ptr, "tfa9890_Set_SpeakerNeeded");
+    *(void **)&tfa9890_dev->stereo_speaker_on = dlsym(tfa9890_dev->lib_ptr, "tfa9890_Stereo_SpeakerOn");
+    *(void **)&tfa9890_dev->stereo_speaker_off = dlsym(tfa9890_dev->lib_ptr, "tfa9890_Stereo_SpeakerOff");
+    *(void **)&tfa9890_dev->stereo_speaker_needed = dlsym(tfa9890_dev->lib_ptr, "tfa9890_Set_Stereo_SpeakerNeeded");
+    *(void **)&tfa9890_dev->speaker_on_bypass = dlsym(tfa9890_dev->lib_ptr, "tfa9890_Stereo_SpeakerOn_bypass");
+    *(void **)&tfa9890_dev->speaker_off_bypass = dlsym(tfa9890_dev->lib_ptr, "tfa9890_Stereo_SpeakerOff_bypass");
     *(void **)&tfa9890_dev->set_mode = dlsym(tfa9890_dev->lib_ptr, "tfa9890_set_mode");
     *(void **)&tfa9890_dev->set_device = dlsym(tfa9890_dev->lib_ptr, "tfa9890_set_device");
-    if (!tfa9890_dev->set_mode || !tfa9890_dev->set_device) {
+    if (!tfa9890_dev->speaker_needed || !tfa9890_dev->stereo_speaker_on ||
+        !tfa9890_dev->stereo_speaker_off || !tfa9890_dev->set_mode || !tfa9890_dev->set_device ||
+        !tfa9890_dev->stereo_speaker_needed || !tfa9890_dev->speaker_on_bypass ||
+        !tfa9890_dev->speaker_off_bypass) {
         ALOGE("%s:%d: Unable to find required symbols", __func__, __LINE__);
         dlclose(tfa9890_dev->lib_ptr);
         free(tfa9890_dev);
         return -ENODEV;
     }
+
+    tfa9890_switch_amp_control_mode(tfa9890_dev);
 
     sem_init(&tfa9890_dev->trigger_update, 0, 0);
     sem_init(&tfa9890_dev->trigger_exit, 0, 0);
