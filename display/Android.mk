kranone124/android_device_zte_axon7-1

axon7: Move qdcm calibration file to /vendor
Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

display/Android.mk
@@ -12,5 +12,6 @@ LOCAL_MODULE       := qdcm_calib_data_zteSAM_S6E3HA3_SAM_25601440_5P5Inch.xml
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := qdcm_calib_data_zteSAM_S6E3HA3_SAM_25601440_5P5Inch.xml
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 

LineageOS/android_device_zte_axon7

display/Android.mk
@@ -12,5 +12,6 @@ LOCAL_MODULE       := qdcm_calib_data_zteSAM_S6E3HA3_SAM_25601440_5P5Inch.xml
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := qdcm_calib_data_zteSAM_S6E3HA3_SAM_25601440_5P5Inch.xml
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 