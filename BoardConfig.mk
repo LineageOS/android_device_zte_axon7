2e6ae50f062df2c20e6221d4e681ea9ab4b044f5
c457 <android.c357@gmail.com>
2018-05-14 19:42:29 -0500

    axon7: sepolicy: Scrap old policies
    * Let's start from scratch to ensure we have everything on point
    and i'll rebase this commit later.
    * Include qcom sepolicy so we aren't flooded with denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -208,7 +208,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_axon7
 TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 
 #BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
