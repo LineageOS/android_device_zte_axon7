600510dff8e3401dd82fffa515c836b11f1aebb7
C457 <android.c357@gmail.com>
2018-08-09 13:14:12 -0500

    axon7: Move thermal configuration to vendor
    * thermal-engine is part of vendor

--- device.mk
+++ device.mk
@@ -390,9 +390,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-8996-normal.conf:system/etc/thermal-engine-8996-normal.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-8996-perf.conf:$system/etc/thermal-engine-8996-perf.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-8996-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-normal.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-8996-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-perf.conf
 
 # VNDK
 PRODUCT_PACKAGES += \
