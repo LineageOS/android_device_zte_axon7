4c0dfb2d96e2f4ae4b65e544ecd1d1d6e413972f
kevte89 <39636900+kevte89@users.noreply.github.com>
2018-09-25 01:25:52 +0200

    Update and rename candy.mk to aicp.mk

--- aicp.mk
+++ aicp.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ailsa_ii device
 $(call inherit-product, device/zte/axon7/device.mk)
 
-# Inherit some common Candy stuff.
-$(call inherit-product, vendor/candy/config/common_full_phone.mk)
+# Inherit some common Aicp stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := candy_axon7
+PRODUCT_NAME := aicp_axon7
 PRODUCT_DEVICE := axon7
 PRODUCT_BRAND := ZTE
 PRODUCT_MODEL := ZTE A2017U
