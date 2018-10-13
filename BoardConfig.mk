6ca0190d9be87a5b2d16ca27cde79bd51d5bba68
Druboo666 <drubo777@gmail.com>
2018-10-14 02:48:29 +0600

    axon7:change defconfig to EAS
    Change-Id: I310f5c019a6376a812a3835e59be0fba533b1b66

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/zte/msm8996
-TARGET_KERNEL_CONFIG := lineageos_axon7_defconfig
+TARGET_KERNEL_CONFIG := eas_axon7_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Qualcomm support
